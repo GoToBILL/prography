3d16157cdd2b5bbe006369636a9a642581dea486
GoToBILL <wnqudwnrlrl@naver.com>
2025-02-20 20:01:01 +0900

    feat: swagger, RestTemplate add fakerApi 요청을 받기 위해서 restTemplate add

---
+++ src/main/java/com/example/prography_project/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package com.example.prography_project.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Prography Project API")
+                        .version("1.0")
+                        .description("Prography 프로젝트 API 문서"));
+    }
+}
